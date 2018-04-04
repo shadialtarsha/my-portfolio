c88b2e0fb6208871402ef0a3170f8b5c5b9cfadc
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-04-04 05:56:50 +0300

    Add Code style

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SocialMedia = () => (
+  <div className="social-media">
+    <a href="https://www.facebook.com/shadi.altarsha" target="_blank" rel="noopener noreferrer">
+      <i className="social-media__icon fb fab fa-facebook-square" />
+    </a>
+    <a href="https://github.com/shadialtarsha" target="_blank" rel="noopener noreferrer">
+      <i className="social-media__icon github fab fa-github-square" />
+    </a>
+    <a href="https://twitter.com/shadi_altarsha" target="_blank" rel="noopener noreferrer">
+      <i className="social-media__icon twitter fab fa-twitter-square" />
+    </a>
+  </div>
+);
+
+export default SocialMedia;
