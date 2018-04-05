c4d327eed88ce4af94ca4313d1408ea8e26728b0
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-04-05 06:44:27 +0300

    Add NotFoundPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from './Header';
 
 const NotFoundPage = () => (
-  <div>
-    <Header />
-    <div>
-      <h1>404 error</h1>
-      <p>Wow!Something went wrong.</p>
-      <p>The page you are trying to access does not exist.</p>
-      <p>Click on the link below to return to the website.</p>
-      <Link to="/">Return to the website</Link>
-    </div>
-  </div>
+  <main className="page-content">
+    <section className="current">
+      <div className="pages" id="error">
+        <section className="centered">
+          <div className="wrapper">
+            <header>
+              <h1>404 error</h1>
+              <p>Wow! Something went wrong.</p>
+              <p>The page you are trying to access does not exist.</p>
+              <p>Click on the link below to return to the website.</p>
+              <Link to="/about">Return to the website</Link>
+            </header>
+          </div>
+        </section>
+      </div>
+    </section>
+  </main>
 );
 
 export default NotFoundPage;
