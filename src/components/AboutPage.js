638bb8dce1a08ef86da325c3d968d5dcfaf282b3
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-04-16 06:46:26 +0300

    Style AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -6,27 +6,43 @@ import SocialMedia from './SocialMedia';
 const AboutPage = () => (
   <div>
     <Menu />
-    <main>
-      <SocialMedia />
-      <div>
-        <div>
-          <img src="/images/avatar.jpg" alt="me" width="200" height="200" />
+    <div className="page">
+      <main className="centered" id="about">
+        <div className="wrapper">
+          <div className="about__container">
+            <div className="avatar">
+              <img src="/images/avatar.jpg" alt="me" width="200" height="200" />
+            </div>
+            <div>
+              <h3>Hello, My name is Shadi Al-Tarsha.</h3>
+              <p>
+                <span id="special-word">I am a Software developer</span>, ambitious, self-motivated, team worker, and a
+                big believer in the self-taught approach.
+              </p>
+              <p>
+                My favorite programming language is JavaScript, but Java was my best friend for the first 2 years of my
+                career.
+              </p>
+              <p>
+                Check out my projects in the{' '}
+                <NavLink id="special-link" to="/portfolio">
+                  portfolio page
+                </NavLink>.
+              </p>
+              <p>
+                If you want to make something exsiting together, feel free to email me at{' '}
+                <span id="special-word">work@shadialtarsha.com</span> or fill a form in the{' '}
+                <NavLink id="special-link" to="/contact">
+                  contact page
+                </NavLink>.
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h3>Hello, My name is Shadi Al-Tarsha.</h3>
-          <p>
-            Graduated as a Software Engineer. Ambitious, self-motivated, team worker, and a big believer in self-taught
-            approach.
-          </p>
-          <p>
-            My favorite programming language is JavaScript, but Java was my best friend for the first 2 years of my
-            career.
-          </p>
-          <p>If you want to make something exsiting together, feel free to email me at shadi.altarsha.94@gmail.com</p>
-        </div>
-      </div>
-    </main>
-    <NavLink to="/skills">skills</NavLink>
+      </main>
+    </div>
+    {/* <NavLink to="/skills">skills</NavLink> */}
+    <SocialMedia />
   </div>
 );
 
