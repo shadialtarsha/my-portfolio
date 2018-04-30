d05c0643ece87bce9b09aeef70dbc4567b7fe1e5
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-04-30 15:52:43 +0300

    Fix skills animation

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const SkillItem = ({ skill }) => (
-  <div className="content">
-    <div className="group">
-      <div className={`circular-progress${skill.percentage.split('%')[0]} circle`} />
-      <div className="circular-text">{skill.name}</div>
-    </div>
-  </div>
-);
+const createBackgroundColor = (percent, barColor) => {
+  const increment = 360 / 100;
+  const half = Math.round(100 / 2);
+  let backgroundImage;
+  if (percent < half) {
+    const nextDeg = 90 + increment * percent;
+    backgroundImage = `linear-gradient(90deg, #E0E0E0 50%, transparent 50%, transparent),
+    linear-gradient(${nextDeg}deg, ${barColor} 50%, #E0E0E0 50%, #E0E0E0)`;
+  } else {
+    const nextDeg = -90 + increment * (percent - half);
+    backgroundImage = `linear-gradient(${nextDeg}deg, ${barColor} 50%, transparent 50%, transparent),
+      linear-gradient(270deg, ${barColor} 50%, #E0E0E0 50%, #E0E0E0)`;
+  }
+  return backgroundImage;
+};
 
-export default SkillItem;
+class SkillItem extends React.Component {
+  componentDidMount() {
+    console.log(this.props.history);
+    const percentage = this.props.skill.percentage.split('%')[0];
+    this.animateCircle(percentage);
+  }
+
+  animateCircle(percentage) {
+    const duration = this.props.history.action === 'PUSH' ? 2800 : 1500;
+    const circle = this.circleRef.current;
+    circle.setAttribute('data-content', '0%');
+    setTimeout(() => {
+      let counter = 0;
+      const intervalId = setInterval(() => {
+        if (counter <= percentage) {
+          circle.style.backgroundImage = createBackgroundColor(counter, '#61afef');
+          circle.setAttribute('data-content', `${counter}%`);
+          counter++;
+        }
+      }, percentage / 1000);
+      setTimeout(() => {
+        clearInterval(intervalId);
+      }, 2000);
+    }, duration);
+  }
+
+  circleRef = React.createRef();
+
+  render() {
+    return (
+      <div className="content">
+        <div className="group">
+          <div ref={this.circleRef} className="circle" />
+          <div className="circular-text">{this.props.skill.name}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SkillItem);
